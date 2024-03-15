499352dc72240ee71ce66cbadfbab03af98efb7f
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-15 11:55:17 +0600

    Create Fake Data For The Blogs And Load Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl bg-red-200'>Knowkedge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Knowkedge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
