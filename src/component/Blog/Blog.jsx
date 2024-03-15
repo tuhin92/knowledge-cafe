ed8006d94facb1829bc31826ca9e7a1295e2dd3f
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-15 19:03:43 +0600

    Display Blog Data And Hashtags

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,8 +1,25 @@
-const Blog = (blog) => {
-    console.log(blog);
+import PropTypes from 'prop-types';
+const Blog = ({blog}) => {
+    const {title, cover, reading_time, author, author_img, posted_date, hashtag} = blog;
     return (
         <div>
-            
+            <img src={cover} alt="" style={{ maxWidth: '100%', maxHeight: '60vh' }} />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    <div>
+                        <img className='w-14' src={author_img} alt="" />
+                    </div>
+                    <div className='ml-6'>
+                        <h3 className='text-2xl '>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-3xl">{title}</h2>
+            <h4>{hashtag}</h4>
         </div>
     );
 };
--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 import { data } from "autoprefixer";
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -13,7 +14,13 @@ const Blogs = () => {
 
     return (
         <div className="md:w-2/3">
-           <h1 className="text-4xl">Blogs: {blogs.length}</h1> 
+           <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+           {
+                blogs.map(blog => <Blog 
+                    key={blog.id} 
+                    blog ={blog}>
+                </Blog>)
+           } 
         </div>
     );
 };
